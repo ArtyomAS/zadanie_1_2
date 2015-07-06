7c35c5bb7bc4836892f5c18c3164aba4b332e68a
ArtyomAS <9jack.daniels9@mail.ru>
2015-07-06 12:34:12 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -18,10 +18,10 @@ namespace zadanie_1_2
             Console.WriteLine("введите ординату точки: ");
             y = double.Parse(Console.ReadLine());
             r = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
-            if (x < 0 && y > 0 && r <= 1)
+            if (x < 0 && y > 0 && r <= 1){
                 Console.WriteLine(inside);
             if (x > 0 && y > 0 && r <= 1 && r >= 0.5)
-                Console.WriteLine(inside);
+                Console.WriteLine(inside);}
             else
                 Console.WriteLine(outside);
 
